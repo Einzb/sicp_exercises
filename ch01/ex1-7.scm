93161d906696c28e6e5c0648bf2acc95fb4fec75
PM_Einz <jason199786109@gmail.com>
2018-05-09 11:15:10 +0800

    done

---
+++ ch01/ex1-7.scm
@@ -0,0 +1,20 @@
+(define (square n)
+  (* n n))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess next-guess)
+  (< (abs (- guess next-guess)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess (improve guess x))
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (my-sqrt n)
+  (sqrt-iter 1.0 n))
+		    
