76fad719cc8495b27ec7fb91bc7492e86233a5a0
PM_Einz <jason199786109@gmail.com>
2018-05-12 23:23:09 +0800

    done

--- ch01/ex1-11.scm
+++ ch01/ex1-11.scm
@@ -7,3 +7,18 @@
 	    (f-recur (- n 2)))
 	 (* 3
 	    (f-recur (- n 3))))))
+
+;; iterative way
+(define (f-iter n)
+  (define (iter fn-1 fn-2 fn-3 counter)
+    (if (= counter n)
+	fn-1
+	(iter (+ fn-1
+		 (* 2 fn-2)
+		 (* 3 fn-3))
+	      fn-1
+	      fn-2
+	      (+ 1 counter))))
+    (if (< n 3)
+	n
+	(iter 2 1 0 2)))
