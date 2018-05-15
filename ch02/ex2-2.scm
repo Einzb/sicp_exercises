906686787aaea3bdfa5a372ab8178cb2f1d805cd
PM_Einz <jason199786109@gmail.com>
2018-05-15 14:53:06 +0800

    done

---
+++ ch02/ex2-2.scm
@@ -0,0 +1,42 @@
+;; point
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+
+;; segment
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (midpoint-segment s)
+  (define (average a b)
+    (/ (+ a b)
+       2))
+  (let ((p1 (start-segment s))
+	(p2 (end-segment s)))
+	(cons (average (x-point p1)
+		       (x-point p2))
+	      (average (y-point p1)
+		       (y-point p2)))))
+
