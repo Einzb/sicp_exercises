18f6c961f674d04a57f5089bb28f4e57abcfff8e
PM_Einz <jason199786109@gmail.com>
2018-05-19 20:06:42 +0800

    done

---
+++ ch02/ex2-31.scm
@@ -0,0 +1,12 @@
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (tree-map proc sub-tree)
+	     (proc sub-tree)))
+       tree))
+
+(define (square n)
+  (* n n))
+
+(define (square-tree tree)
+  (tree-map square tree))
