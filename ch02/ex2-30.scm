417a326dbc3ddee693875d819c87b0e1564f90c0
PM_Einz <jason199786109@gmail.com>
2018-05-19 19:51:27 +0800

    both way done

--- ch02/ex2-30.scm
+++ ch02/ex2-30.scm
@@ -3,12 +3,21 @@
 (define (square n)
   (* n n))
 
-(define (square-tree tree)
+;; define square-tree directly(i.e., without using any higher-order procedures)
+(define (square-tree-directly tree)
   (cond ((null? tree) nil)
 	((not (pair? tree))
 	 (square tree))
 	(else
 	 (cons (square-tree (car tree))
 	       (square-tree (cdr tree))))))
+
+;; define square-tree by using map and recursion
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (square-tree sub-tree)
+	     (square sub-tree)))
+       tree))
 	 
 	      
